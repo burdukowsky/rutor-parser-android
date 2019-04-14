fbaf4d141236f7ffbd6939053b3ceea6cd3df619
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2019-04-14 11:47:44 +0700

    implement searchView
    what was done:
    - changed main layout to LinearLayout
    - added options_menu
    - added search_hint string
    - updated manifest
    - deleted constraint-layout from build.gradle
    - added searchable to res/xml
    - implemented handling Intent.ACTION_SEARCH

--- app/src/main/java/tk/burdukowsky/rutorparserandroid/MainActivity.kt
+++ app/src/main/java/tk/burdukowsky/rutorparserandroid/MainActivity.kt
@@ -1,12 +1,43 @@
 package tk.burdukowsky.rutorparserandroid
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.apply {
+            setIconifiedByDefault(false)
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            // doMySearch(query)
+        }
     }
 }
