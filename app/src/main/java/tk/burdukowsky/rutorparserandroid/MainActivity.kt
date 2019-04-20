933f69e8d095ea1eace8779221a7761531250083
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2019-04-20 22:54:08 +0700

    get rid of redundant AsyncTask

--- app/src/main/java/tk/burdukowsky/rutorparserandroid/MainActivity.kt
+++ app/src/main/java/tk/burdukowsky/rutorparserandroid/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
-import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -20,6 +19,8 @@ import retrofit2.Response
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val apiService: ApiService = ApiServiceProvider.instance
 
     private lateinit var linearLayoutProgress: LinearLayout
     private lateinit var list: ListView
@@ -58,76 +59,72 @@ class MainActivity : AppCompatActivity() {
     private fun handleIntent(intent: Intent) {
         if (Intent.ACTION_SEARCH == intent.action) {
             val query = intent.getStringExtra(SearchManager.QUERY)
-            GetResultsTask(this).execute(query)
+            getResults(query)
         }
     }
 
-    private class GetResultsTask
-    internal constructor(context: MainActivity) : AsyncTask<String, Void, Call<List<Result>>>() {
+    private fun getResults(query: String) {
+        setViewVisibility(this.linearLayoutProgress, true)
+        setViewVisibility(this.empty, false)
+        setViewVisibility(this.errorMessage, false)
 
-        private val activityReference: WeakReference<MainActivity> = WeakReference(context)
-        private val apiService: ApiService = ApiServiceProvider.instance
+        val context = this
 
-        override fun onPreExecute() {
-            val activity = activityReference.get()
-            if (activityEnded(activity)) return
-            activity!!
-            setViewVisibility(activity.linearLayoutProgress, true)
-            setViewVisibility(activity.empty, false)
-            setViewVisibility(activity.errorMessage, false)
-        }
+        apiService.getResults(query).enqueue(
+            object : Callback<List<Result>> {
+                private val activityReference: WeakReference<MainActivity> = WeakReference(context)
 
-        override fun doInBackground(vararg params: String): Call<List<Result>> {
-            return apiService.getResults(params[0])
-        }
+                override fun onResponse(call: Call<List<Result>>, response: Response<List<Result>>) {
+                    val activity = activityReference.get()
+                    if (activityEnded(activity)) return
+                    activity!!
 
-        override fun onPostExecute(call: Call<List<Result>>) {
-            val activity = activityReference.get()
-            if (activityEnded(activity)) return
-            activity!!
-
-            call.enqueue(
-                object : Callback<List<Result>> {
-                    override fun onResponse(call: Call<List<Result>>, response: Response<List<Result>>) {
-                        val results = response.body()
-                        if (response.isSuccessful && results != null) {
-                            activity.list.adapter = ResultListAdapter(activity, results)
-                            setViewVisibility(activity.empty, results.isEmpty())
-                        } else {
-                            var errorMessage = activity.getString(R.string.error_message, response.code())
-                            val errorBody = response.errorBody()
-                            if (errorBody != null) {
-                                val apiError = ErrorConverterProvider.instance.convert(errorBody)
-                                if (apiError != null) {
-                                    errorMessage += ": ${apiError.message}"
-                                }
+                    val results = response.body()
+                    if (response.isSuccessful && results != null) {
+                        activity.list.adapter = ResultListAdapter(activity, results)
+                        setViewVisibility(activity.empty, results.isEmpty())
+                    } else {
+                        var errorMessage = activity.getString(R.string.error_message, response.code())
+                        val errorBody = response.errorBody()
+                        if (errorBody != null) {
+                            val apiError = ErrorConverterProvider.instance.convert(errorBody)
+                            if (apiError != null) {
+                                errorMessage += ": ${apiError.message}"
                             }
-                            // todo: дублирование
-                            activity.errorMessage.text = errorMessage
-                            setViewVisibility(activity.errorMessage, true)
-                            activity.list.adapter = ResultListAdapter(activity, Collections.emptyList())
                         }
-                    }
-
-                    override fun onFailure(call: Call<List<Result>>, t: Throwable) {
                         // todo: дублирование
-                        activity.errorMessage.text = activity.getString(R.string.request_failed_message)
+                        activity.errorMessage.text = errorMessage
                         setViewVisibility(activity.errorMessage, true)
                         activity.list.adapter = ResultListAdapter(activity, Collections.emptyList())
                     }
+
+                    // todo: дублирование
+                    setViewVisibility(activity.linearLayoutProgress, false)
                 }
-            )
 
-            setViewVisibility(activity.linearLayoutProgress, false)
-        }
+                override fun onFailure(call: Call<List<Result>>, t: Throwable) {
+                    val activity = activityReference.get()
+                    if (activityEnded(activity)) return
+                    activity!!
 
-        private fun activityEnded(activity: Activity?): Boolean {
-            return activity == null || activity.isFinishing
-        }
+                    // todo: дублирование
+                    activity.errorMessage.text = activity.getString(R.string.request_failed_message)
+                    setViewVisibility(activity.errorMessage, true)
+                    activity.list.adapter = ResultListAdapter(activity, Collections.emptyList())
 
-        private fun setViewVisibility(view: View, status: Boolean) {
-            view.visibility = if (status) View.VISIBLE else View.GONE
-        }
+                    // todo: дублирование
+                    setViewVisibility(activity.linearLayoutProgress, false)
+                }
+            }
+        )
+    }
+
+    private fun activityEnded(activity: Activity?): Boolean {
+        return activity == null || activity.isFinishing
+    }
+
+    private fun setViewVisibility(view: View, status: Boolean) {
+        view.visibility = if (status) View.VISIBLE else View.GONE
     }
 
 }
