ef0a684b9c88d63f9361f79075e05e24396da996
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2019-04-21 01:15:25 +0700

    implement magnet-link

--- app/src/main/java/tk/burdukowsky/rutorparserandroid/ResultListAdapter.kt
+++ app/src/main/java/tk/burdukowsky/rutorparserandroid/ResultListAdapter.kt
@@ -1,11 +1,16 @@
 package tk.burdukowsky.rutorparserandroid
 
+import android.content.ActivityNotFoundException
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 import android.content.Context
 import android.view.LayoutInflater
+import android.content.Intent
+import android.net.Uri
+import android.widget.ImageView
+import android.widget.Toast
 
 class ResultListAdapter(private var context: Context, private var results: List<Result>) : BaseAdapter() {
 
@@ -18,6 +23,20 @@ class ResultListAdapter(private var context: Context, private var results: List<
         view.findViewById<TextView>(R.id.textViewResultSize).text = result.size
         view.findViewById<TextView>(R.id.textViewResultSeeds).text = context.getString(R.string.seeds, result.seeds)
         view.findViewById<TextView>(R.id.textViewResultLeaches).text = context.getString(R.string.leaches, result.leaches)
+        view.findViewById<ImageView>(R.id.imageButtonResultMagnet).setOnClickListener {
+            try {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result.magnet))
+                context.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast
+                    .makeText(
+                        context,
+                        context.getString(R.string.not_found_application),
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
+        }
         return view
     }
 
