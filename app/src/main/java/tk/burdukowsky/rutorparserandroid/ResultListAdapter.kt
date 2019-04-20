7c6cdae19a0ec55b2b643f82a6aabbc2820d2438
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2019-04-20 23:56:12 +0700

    add seeds and leaches to result view + customize theme

--- app/src/main/java/tk/burdukowsky/rutorparserandroid/ResultListAdapter.kt
+++ app/src/main/java/tk/burdukowsky/rutorparserandroid/ResultListAdapter.kt
@@ -7,15 +7,17 @@ import android.widget.TextView
 import android.content.Context
 import android.view.LayoutInflater
 
-class ResultListAdapter(context: Context, private var results: List<Result>) : BaseAdapter() {
+class ResultListAdapter(private var context: Context, private var results: List<Result>) : BaseAdapter() {
 
     private var layoutInflater: LayoutInflater = LayoutInflater.from(context)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View = convertView ?: layoutInflater.inflate(R.layout.result, parent, false)
         val result = getItem(position)
-        (view.findViewById(R.id.textViewResultTitle) as TextView).text = result.title
-        (view.findViewById(R.id.textViewResultSize) as TextView).text = result.size
+        view.findViewById<TextView>(R.id.textViewResultTitle).text = result.title
+        view.findViewById<TextView>(R.id.textViewResultSize).text = result.size
+        view.findViewById<TextView>(R.id.textViewResultSeeds).text = context.getString(R.string.seeds, result.seeds)
+        view.findViewById<TextView>(R.id.textViewResultLeaches).text = context.getString(R.string.leaches, result.leaches)
         return view
     }
 
